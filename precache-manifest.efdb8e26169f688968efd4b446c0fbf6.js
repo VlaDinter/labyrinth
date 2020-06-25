b9b80fff396bd45cbe58fa718c040f0721eace44
VlaDinter <vladyslavdinter@gmail.com>
2020-06-25 10:38:00 +0300

    Updates

---
+++ precache-manifest.efdb8e26169f688968efd4b446c0fbf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f20892618631a752134b3c3ed385e91e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c9529c2017dd560bc803",
+    "url": "./static/css/main.ab6f33f3.chunk.css"
+  },
+  {
+    "revision": "9764490313b79841c50f",
+    "url": "./static/js/2.391d3d7d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.391d3d7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9529c2017dd560bc803",
+    "url": "./static/js/main.2ac06182.chunk.js"
+  },
+  {
+    "revision": "3f1ac0e86cd582ea8fb0",
+    "url": "./static/js/runtime-main.04f1e90a.js"
+  }
+]);
